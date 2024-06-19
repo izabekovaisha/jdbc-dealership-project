705f1ed70cfc681a47d1b03f5db731b901818dca
izabekovaisha <izabekovaisha@gmail.com>
2024-06-19 18:03:11 -0400

    Add addSalesContract()

--- src/main/java/com/yearup/dealership/db/SalesDao.java
+++ src/main/java/com/yearup/dealership/db/SalesDao.java
@@ -3,9 +3,7 @@ package com.yearup.dealership.db;
 import com.yearup.dealership.models.SalesContract;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class SalesDao {
     private DataSource dataSource;
@@ -15,6 +13,25 @@ public class SalesDao {
     }
 
     public void addSalesContract(SalesContract salesContract) {
-        // TODO: Implement the logic to add a sales contract
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(
+                     "INSERT INTO sales_contracts (VIN, sale_date, price) VALUES (?, ?, ?)",
+                     PreparedStatement.RETURN_GENERATED_KEYS)) {
+
+            statement.setString(1, salesContract.getVin());
+            statement.setDate(2, Date.valueOf(salesContract.getSaleDate()));
+            statement.setDouble(3, salesContract.getPrice());
+            statement.executeUpdate();
+
+            try (ResultSet generatedKeys = statement.getGeneratedKeys();) {
+                if (generatedKeys.next()) {
+                    int contractId = generatedKeys.getInt(1);
+                    System.out.println("New sales contract ID: " + contractId);
+                }
+            }
+        } catch (Exception e) {
+            System.err.println("Error occurred while adding the sales contract: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
